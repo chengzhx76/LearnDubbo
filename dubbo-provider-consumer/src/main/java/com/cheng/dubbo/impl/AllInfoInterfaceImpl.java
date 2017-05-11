3254342800e335a34db0fe79ea8664eb66bb0a4e
chengzhx76 <chengzhx76@qq.com>
2017-05-11 10:39:12 +0800

    优化

--- dubbo-provider-consumer/src/main/java/com/cheng/dubbo/impl/AllInfoInterfaceImpl.java
+++ dubbo-provider-consumer/src/main/java/com/cheng/dubbo/impl/AllInfoInterfaceImpl.java
@@ -14,6 +14,7 @@ import javax.annotation.Resource;
 public class AllInfoInterfaceImpl implements AllInfoInterface {
     @Resource
     private GrowUpInterface growUpInterface;
+
     public People getAllInfo(People people) {
         people = growUpInterface.addAge(people);
         people.setAge(99);
