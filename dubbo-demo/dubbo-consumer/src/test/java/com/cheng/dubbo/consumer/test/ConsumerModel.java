5698226cd95ea31f9d953a487c05120cb2a8d1ca
chengzhx76 <chengzhx76@qq.com>
2019-09-09 21:53:00 +0800

    motan model add demo

--- dubbo-demo/dubbo-consumer/src/test/java/com/cheng/dubbo/consumer/test/ConsumerModel.java
+++ dubbo-demo/dubbo-consumer/src/test/java/com/cheng/dubbo/consumer/test/ConsumerModel.java
@@ -15,10 +15,11 @@ public class ConsumerModel {
 
     public static void main(String[] args) {
         ReferenceConfig<NameInterface> reference = new ReferenceConfig<>();
-        reference.setApplication(new ApplicationConfig("first-dubbo-consumer"));
-        reference.setRegistry(new RegistryConfig("zookeeper://139.196.35.134:2181"));
+        reference.setApplication(new ApplicationConfig("cheng-dubbo-consumer"));
+        reference.setRegistry(new RegistryConfig("zookeeper://139.196.35.134:2181", "zookeeper"));
         reference.setInterface(NameInterface.class);
         NameInterface service = reference.get();
+
         String message = service.getName(new People());
         System.out.println(message);
     }
--- dubbo-demo/dubbo-provider/src/main/java/com/cheng/dubbo/start/ProviderModel.java
+++ dubbo-demo/dubbo-provider/src/main/java/com/cheng/dubbo/start/ProviderModel.java
@@ -2,9 +2,7 @@ package com.cheng.dubbo.start;
 
 import com.cheng.dubbo.NameInterface;
 import com.cheng.dubbo.impl.NameInterfaceImpl;
-import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.RegistryConfig;
-import org.apache.dubbo.config.ServiceConfig;
+import org.apache.dubbo.config.*;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -17,8 +15,9 @@ public class ProviderModel {
 
     public static void main(String[] args) throws Exception {
         ServiceConfig<NameInterface> service = new ServiceConfig<>();
-        service.setApplication(new ApplicationConfig("first-dubbo-provider"));
-        service.setRegistry(new RegistryConfig("zookeeper://139.196.35.134:2181"));
+        service.setApplication(new ApplicationConfig("dubbo-provider"));
+        service.setRegistry(new RegistryConfig("zookeeper://139.196.35.134:2181", "zookeeper"));
+        service.setProtocol(new ProtocolConfig("dubbo", 20886));
         service.setInterface(NameInterface.class);
         service.setRef(new NameInterfaceImpl());
         service.export();
